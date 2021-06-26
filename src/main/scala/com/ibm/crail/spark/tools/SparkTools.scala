aa9d8ad7bcad874ab25817520fa85ff5212a1853
Malte Brodmann <mbrodmann@ethz.ch>
2021-06-26 17:03:06 +0000

    adapt code and dependencies for latest spark version

--- src/main/scala/com/ibm/crail/spark/tools/SparkTools.scala
+++ src/main/scala/com/ibm/crail/spark/tools/SparkTools.scala
@@ -9,7 +9,7 @@ object SparkTools {
   def countNumPartitions(spark: SparkSession, x:Dataset[_]): Long = {
     /* count how many partitions does it have */
     val accum = spark.sparkContext.longAccumulator("resultPartition")
-    x.foreachPartition(p => accum.add(1))
+    x.foreachPartition((p: Iterator[_]) => accum.add(1))
     accum.value
   }
 }
